1d9dab7a51a7f314ac107cb0286611408eb5cc3e
Abhi <abhishekchauhan9036@gmail.com>
2022-06-08 20:41:12 +0530

    commit message

---
+++ Age-Limit.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define int long long 
+int32_t main(){
+	int Bhosdike;
+	cin>>Bhosdike;
+	while(Bhosdike--){
+        int ___OOOO,_PPPP,____KKKK,__;
+        cin>>___OOOO>>_PPPP>>____KKKK;
+
+        if(____KKKK>=___OOOO && ____KKKK<_PPPP){
+            cout<<"Yes"<<endl;
+        }else{
+            cout<<"No"<<endl;
+        }
+        
+	}
+	return 0;
+}
