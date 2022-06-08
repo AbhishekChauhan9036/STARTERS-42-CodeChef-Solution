0dfc1b9056ae82a0c0261f9652e6f46ad4a5e14c
Abhi <abhishekchauhan9036@gmail.com>
2022-06-08 20:44:26 +0530

    commit message

---
+++ Equal-Distribution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define int long long 
+int32_t main(){
+	int Bhosdike;
+	cin>>Bhosdike;
+	while(Bhosdike--){
+        int ___OOOO,_PPPP,____KKKK,__;
+        cin>>___OOOO>>_PPPP;
+
+        if((___OOOO+_PPPP)%2==0){
+            cout<<"Yes"<<endl;
+        }else{
+            cout<<"No"<<endl;
+        }
+        
+	}
+	return 0;
+}
